bffea776ff747d9e0b761b69215cdc4a218eb494
ailingomez96 <gomezailin96@gmail.com>
2024-08-19 13:33:36 -0300

    cambios formulario

--- formulario.js
+++ formulario.js
@@ -1,29 +1,46 @@
 //LOCAL STORAGE
 
 document.addEventListener('DOMContentLoaded', () => {
-
     const formulario = document.getElementById('miFormulario');
 
     let datosGuardados = JSON.parse(localStorage.getItem('formularioData'));
     if (datosGuardados && Array.isArray(datosGuardados)) {
-
         console.log('Datos guardados:', datosGuardados);
     } else {
-
         datosGuardados = [];
     }
 
-
     formulario.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevenir el envío del formulario
 
+        let errores = [];
+        let inputNombre = document.querySelector("input#nombre");
+
+        if (inputNombre.value === "") {
+            errores.push("El campo nombre tiene que estar completo");
+        } else if (inputNombre.value.length < 5) {
+            errores.push("El campo nombre debe tener al menos 5 caracteres");
+        }
+
+        //MUESTRA EL ERROR EN UNA LINEA DE HTML.
+        // let ulErrores = document.querySelector("div.errores ul");
+        // ulErrores.innerHTML = ''; // Limpiar errores anteriores
+        // if (errores.length > 0) {
+
+        //     event.preventDefault();
+        //     for (let i = 0; i < errores.length; i++) {
+        //         ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
+        //     }
+            
+            alert(errores.join('\n'));
+
+            return;
+        
 
         const datosFormulario = new FormData(formulario);
         const datosObj = {};
         datosFormulario.forEach((valor, clave) => {
             datosObj[clave] = valor;
         });
-
 
         let datosArray = JSON.parse(localStorage.getItem('formularioData'));
         if (!Array.isArray(datosArray)) {
@@ -32,7 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         datosArray.push(datosObj);
 
-
         localStorage.setItem('formularioData', JSON.stringify(datosArray));
 
         alert('Formulario cargado correctamente');
@@ -40,6 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
         formulario.reset();
     });
 });
+
 
 //EVENTOS PARA EL FORMULARIO
 
